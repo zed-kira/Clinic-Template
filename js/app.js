6267eefcf1c274f17dc79643c1aa69b78dc2100b
zed-kira <admin@zedkira.com>
2023-02-18 10:31:40 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// const menuClick = () => {
+//     const menuToggle = document.querySelector('.menu');
+//     const imgClick = document.querySelector('.image');
+
+//     imgClick.addEventListener('click', () => {
+//         menuToggle.classList.toggle('active');
+//     });
+// };
+
+// menuClick();
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "cards",
+  grabCursor: true,
+});
